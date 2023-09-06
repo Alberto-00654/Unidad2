4d199b6d987b3345b4cbb15d9d85cfd8d532d134
Alberto-00654 <143623063+Alberto-00654@users.noreply.github.com>
2023-09-06 08:49:11 -0600

    Add files via upload

---
+++ Tareas.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tareas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Tareas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      String tarea1 = " hacer tarea ";
+      String tarea2 = " sacar a pasear a mi perro ";
+      String tarea3 = " hacer la comida ";
+      String tarea4 = " lavar los trastes ";
+      
+      System.out.println("Hola mi nombre es Alberto y mi Mama me puso estas tareas para el dia de hoy;" + tarea1 + tarea2 + tarea3 + tarea4);
+    }
+    
+}
