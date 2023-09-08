292ac230aa99e9d7af615d0d1fe3f5b3f853e0db
Alberto-00654 <143623063+Alberto-00654@users.noreply.github.com>
2023-09-08 08:52:23 -0600

    Add files via upload

---
+++ AreaTriangulo.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package area.triangulo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AreaTriangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         // Declarar variables
+        double base, altura, area;
+
+
+        // Calcular el área del triángulo
+      
+        base = 10.5;
+        altura = 15;
+         area = (base * altura) / 2;
+        // Mostrar el resultado
+        System.out.println("El área del triángulo es: " + area);
+
+   
+        
+   
+    }
+    
+}
